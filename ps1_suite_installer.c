56b9628d4297d628d34fa4122227dd0f727f5585
juanjose <juanjose.solorzano.c@gmail.com>
2025-01-27 22:08:46 -0600

    Updated: checking if the url exist.

--- ps1_suite_installer.c
+++ ps1_suite_installer.c
@@ -1,17 +1,30 @@
+/**
+ * Author: Solorzano, Juan Jose.
+ * Data: 01/01/2024
+ * Information: ...
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <windows.h>
+#include <sys/stat.h> // folder exists
+#include <windows.h> // sleep and time.h file
 #include <time.h>
+#include <string.h> // args manager.
 
 #define PS1_COMMAND "powershell -C start-process %s"
-#define URL_API "https://github.com/PowerShell/PowerShell/releases/download/v7.4.7/PowerShell-7.4.7-win-x64.zip"
+#define PS1_VERSION "7.4.7"
+#define URL_API "https://github.com/PowerShell/PowerShell/releases/download/v%s/PowerShell-%s-win-x64.zip"
 #define PWSH_SITE "https://github.com/JuanJoseSoloraznoCarrillo/PowerShell/archive/refs/heads/master.zip"
 #define FOLDER_INST "C:\\LegacyApp\\powershell"
 
 /**
  * 
  */
+int url_request(const char *link){
+    char command[512];
+    char *cmd_request = "powershell -C \"try{$req=Invoke-WebRequest -Uri '%s' -UseBasicParsing -Method Head; exit 0} catch{exit 1}\"";
+    sprintf(command,cmd_request,link);
+    return system(command) == 0;
+}
 int folder_exists(char *path){
     struct stat st;
     int exists = stat(path,&st) == 0 && S_ISDIR(st.st_mode);
@@ -65,25 +78,47 @@ int download_pwsh_suite(char *buffer){
     unzip("C:/LegacyApp/powershell/PowerShell-master.zip","C:/LegacyApp/powershell/");
 }
 
+/**
+ * Main
+ */
 int main(int argc,char *argv[]){
-    char cmd_buffer[MAX_PATH];
-    if(!download_pwsh(cmd_buffer)){
-        printf("[!] Error: while download the PowerShell-7.4.7-win-x64.zip");
-        return 1;
+    char *version = NULL;
+    if(argc>1){
+        if(strcmp(argv[1],"version")||strcmp(argv[1],"-version")||strcmp(argv[1],"v")||strcmp(argv[1],"-v")){
+            version = argv[2];
+        }else{
+            printf("Unknow argument: %s\n",argv[1]);
+            version = PS1_VERSION;
+        }
     }
-    if(!folder_exists(FOLDER_INST)){
-       create_folder(cmd_buffer);
+    char url_api[MAX_PATH];
+    sprintf(url_api,URL_API,PS1_VERSION,PS1_VERSION);
+    int exist = url_request(url_api);
+    if(exist==1){
+        printf("Url exist....\n");
     }else{
-        printf("Powershell installation found, removing folder ...\n");
-        sprintf(cmd_buffer,"rmdir /s /q %s",FOLDER_INST);
-        system(cmd_buffer);
-        clear_buffer(cmd_buffer);
-        create_folder(cmd_buffer);
+        printf("Url does not exists\n");
     }
-    if(!install_pwsh()){
-        return 1;
-    }
-    download_pwsh_suite(cmd_buffer);
+
+    //char cmd_buffer[MAX_PATH]; //Command string buffering.
+    //if(!download_pwsh(cmd_buffer)){
+    //    // was not possible to download the 
+    //    printf("[!] Error: while download the PowerShell-7.4.7-win-x64.zip");
+    //    return 1;
+    //}
+    //if(!folder_exists(FOLDER_INST)){
+    //   create_folder(cmd_buffer);
+    //}else{
+    //    printf("Powershell installation found, removing folder ...\n");
+    //    sprintf(cmd_buffer,"rmdir /s /q %s",FOLDER_INST);
+    //    system(cmd_buffer);
+    //    clear_buffer(cmd_buffer);
+    //    create_folder(cmd_buffer);
+    //}
+    //if(!install_pwsh()){
+    //    return 1;
+    //}
+    //download_pwsh_suite(cmd_buffer);
 }
 
 
