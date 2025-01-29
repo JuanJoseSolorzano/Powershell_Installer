be8fe0b4747b72170039c35dfd0a2da782b37982
juanjose <juanjose.solorzano.c@gmail.com>
2025-01-28 22:00:21 -0600

    Updated: <ps1_file.c> - Almost there, it is just pending the script structure.

--- ps1_suite_installer.c
+++ ps1_suite_installer.c
@@ -9,21 +9,32 @@
 #include <windows.h> // sleep and time.h file
 #include <time.h>
 #include <string.h> // args manager.
-
+#define PS1_PROFILE_FILE "C:/LegacyApp/powershell/Microsoft.PowerShell_profile.ps1"
 #define PS1_COMMAND "powershell -C start-process %s"
-#define PS1_VERSION "7.4.7"
 #define URL_API "https://github.com/PowerShell/PowerShell/releases/download/v%s/PowerShell-%s-win-x64.zip"
 #define PWSH_SITE "https://github.com/JuanJoseSoloraznoCarrillo/PowerShell/archive/refs/heads/master.zip"
 #define FOLDER_INST "C:\\LegacyApp\\powershell"
+const char *version = "7.4.7";
 
 /**
  * 
  */
-int url_request(const char *link){
+void help_panel(){
+    printf("[?] Usage:\n");
+    printf("\t\tps1Environment.exe [-v | -version | version | v] <version. eg: 7.4.7>\n");
+}
+void init_print(const char *version){
+    printf("**************************************************************************\n");
+    printf("**                    PS1 Suite Configuration\n");
+    printf("**                    PowerShell Version %s\n",version);
+    printf("**************************************************************************\n");
+
+}
+int url_exists(const char *link){
     char command[512];
     char *cmd_request = "powershell -C \"try{$req=Invoke-WebRequest -Uri '%s' -UseBasicParsing -Method Head; exit 0} catch{exit 1}\"";
     sprintf(command,cmd_request,link);
-    return system(command) == 0;
+    return system(command); // output 1 when the link requested does not exists.
 }
 int folder_exists(char *path){
     struct stat st;
@@ -34,30 +45,30 @@ void clear_buffer(char *buffer){
     buffer[0] = '\0';
 }
 int unzip(char target[],char destination[]){
-    printf("Extracting file: %s ...\n",target);
+    printf("[+] Extracting file: %s ...\n",target);
     char cmd_buf[MAX_PATH];
     sprintf(cmd_buf,"tar -xf \"%s\" -C \"%s\"",target,destination);
     system(cmd_buf);
-    printf("Unziped file: %s\n",destination);
+    printf("[+] Unziped file: %s\n",destination);
 }
 int move_file(char target[],char destination[]){
-    printf("Moving file %s to %s\n",target,destination);
+    printf("[+] Moving file %s to %s\n",target,destination);
     char cmd_buf[MAX_PATH];
     sprintf(cmd_buf,"mv %s %s",target,destination);
     system(cmd_buf);
     Sleep(2000);
     return 1;
 }
-int download_pwsh(char *buffer){
-    printf("Downloading PowerShell-7.4.7-win-x64.zip ...");
-    sprintf(buffer,PS1_COMMAND,URL_API);
+int download_pwsh(char *buffer,char *url){
+    printf("[+] Downloading PowerShell-7.4.7-win-x64.zip ...\n");
+    sprintf(buffer,PS1_COMMAND,url);
     system(buffer);
     clear_buffer(buffer);
     Sleep(13000); //waiting for downloaded file.
     return 1;
 }
 int create_folder(char *buffer){
-    printf("Creating installatioin folder: '%s'\n",FOLDER_INST);
+    printf("[+] Creating installatioin folder: '%s'\n",FOLDER_INST);
     sprintf(buffer,"mkdir %s",FOLDER_INST);
     system(buffer);
     clear_buffer(buffer);
@@ -66,61 +77,76 @@ int create_folder(char *buffer){
 int install_pwsh(){
     move_file("%USERPROFILE%\\Downloads\\PowerShell-7.4.7-win-x64.zip","C:\\LegacyApp\\powershell\\");
     unzip("C:/LegacyApp/powershell/PowerShell-7.4.7-win-x64.zip","C:/LegacyApp/powershell/");
-    printf("Powershell installation -> Done.\n");
+    printf("[*] Powershell installation -> Done.\n");
+    printf("========================================================================================\n");
     return 1;
 }
 int download_pwsh_suite(char *buffer){
+    printf("[+] Downloading ps1 suite ...\n");
     sprintf(buffer,PS1_COMMAND,PWSH_SITE);
     system(buffer);
     clear_buffer(buffer);
     Sleep(5000);
     move_file("%USERPROFILE%\\Downloads\\PowerShell-master.zip","C:\\LegacyApp\\powershell\\");
     unzip("C:/LegacyApp/powershell/PowerShell-master.zip","C:/LegacyApp/powershell/");
+    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    printf("+++                             EXECUTION SUCCESS\n");
+    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+}
+int create_ps1_file(){
+    printf("[+] Creating ps1 file ...\n");
+    FILE *file;
+    file = fopen(PS1_PROFILE_FILE,"w");
+    if(file==NULL){
+        printf("[!] Error: Unable to create file.\n");
+        return 1;
+    }
+    fprintf(file,"$profile_prompt = \"C:\\LegacyApp\\powershell\\PowerShell-master\\Profile.ps1\"\nImport-Module -Name $profile_prompt\n");
+    fclose(file);
+    return 0;
 }
 
 /**
  * Main
  */
 int main(int argc,char *argv[]){
-    char *version = NULL;
+    char *arg_version = NULL;
     if(argc>1){
-        if(strcmp(argv[1],"version")||strcmp(argv[1],"-version")||strcmp(argv[1],"v")||strcmp(argv[1],"-v")){
+        if(strcmp(argv[1],"version")==0||strcmp(argv[1],"-version")==0||strcmp(argv[1],"v")==0||strcmp(argv[1],"-v")==0){
             version = argv[2];
         }else{
             printf("Unknow argument: %s\n",argv[1]);
-            version = PS1_VERSION;
+            help_panel();
         }
     }
     char url_api[MAX_PATH];
-    sprintf(url_api,URL_API,PS1_VERSION,PS1_VERSION);
-    int exist = url_request(url_api);
-    if(exist==1){
-        printf("Url exist....\n");
-    }else{
-        printf("Url does not exists\n");
+    sprintf(url_api,URL_API,version,version);
+    if(url_exists(url_api)==1){
+        printf("[!] The version %s given does not exits!!!\n\n",version);
+        return 1;
     }
-
-    //char cmd_buffer[MAX_PATH]; //Command string buffering.
-    //if(!download_pwsh(cmd_buffer)){
-    //    // was not possible to download the 
-    //    printf("[!] Error: while download the PowerShell-7.4.7-win-x64.zip");
-    //    return 1;
-    //}
-    //if(!folder_exists(FOLDER_INST)){
-    //   create_folder(cmd_buffer);
-    //}else{
-    //    printf("Powershell installation found, removing folder ...\n");
-    //    sprintf(cmd_buffer,"rmdir /s /q %s",FOLDER_INST);
-    //    system(cmd_buffer);
-    //    clear_buffer(cmd_buffer);
-    //    create_folder(cmd_buffer);
-    //}
-    //if(!install_pwsh()){
-    //    return 1;
-    //}
-    //download_pwsh_suite(cmd_buffer);
+    init_print(version);
+    char cmd_buffer[MAX_PATH]; //Command string buffering.
+    if(!download_pwsh(cmd_buffer,url_api)){
+        // was not possible to download the 
+        printf("[!] Error: while download the PowerShell-7.4.7-win-x64.zip");
+        return 1;
+    }
+    if(!folder_exists(FOLDER_INST)){
+       create_folder(cmd_buffer);
+    }else{
+        printf("[*] Powershell installation found, removing folder ...\n");
+        sprintf(cmd_buffer,"rmdir /s /q %s",FOLDER_INST);
+        system(cmd_buffer);
+        clear_buffer(cmd_buffer);
+        create_folder(cmd_buffer);
+    }
+    if(!install_pwsh()){
+        return 1;
+    }
+    download_pwsh_suite(cmd_buffer);
+    if(create_ps1_file()==1){
+        return 1;
+    }
+    rename("C:\\LegacyApp\\powershell\\PowerShell-master\\Modules\\Terminal-Icons","C:\\LegacyApp\\powershell\\Modules\\Terminal-Icons");
 }
-
-
-//"$profile_prompt = C:/....Profile.ps1"
-//"Import-Module -Name $profile"
